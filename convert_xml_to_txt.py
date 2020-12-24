5389e7102e11acf929942d9cbcbbcb3707eb4bb4
Jernej Virag <jernej@virag.si>
2020-12-24 21:55:32 +0100

    Add concatenation step to convert script

--- convert_xml_to_txt.py
+++ convert_xml_to_txt.py
@@ -4,6 +4,7 @@ from builtins import bytes
 import os
 import xml.etree.ElementTree as ET
 import glob
+import shutil
 
 """
 This script converts XML dataset from JOS1M to NLTK-preferred plain-text set with tags separated by slashes.
@@ -13,7 +14,7 @@ It DOES NOT escape slashes so be aware of that.
 The files will be grabbed in xml/ directory and will end up in txt/ subdirectory.
 """
 
-def convert_file(filename):
+def convert_xml_file(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
 
@@ -37,9 +38,20 @@ def convert_file(filename):
             f.write(bytes(b'\n\n'))
     f.close()
 
+def concat_outputs():
+    files = glob.glob("txt/jos1M*.txt")
+    output_file = os.path.join("data", "tagged_corpus", "slotag.txt")
+    print("Concatenating to", output_file)
+    with open(output_file, "wb") as out_f:
+        for f in files:
+            with open(f, "rb") as in_f:
+                shutil.copyfileobj(in_f, out_f)
 
 if __name__ == "__main__":
+    print("Converting XML files to TXT format...")
     files = glob.glob('xml/jos1M*.xml')
     for f in files:
-        convert_file(f)
+        convert_xml_file(f)
+    print("Concatenating TXT files into a single output...")
+    concat_outputs()
     print("Complete.")
