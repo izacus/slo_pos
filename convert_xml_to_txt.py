3ace245efb82409f43f36366ccee24ebde8b5391
Jernej Virag <jernej@virag.si>
2020-12-24 20:41:56 +0100

    Make convert_xml_to_txt script Python 3 compatible

--- convert_xml_to_txt.py
+++ convert_xml_to_txt.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+from builtins import bytes
+
+import os
 import xml.etree.ElementTree as ET
 import glob
 
@@ -5,13 +9,17 @@ import glob
 This script converts XML dataset from JOS1M to NLTK-preferred plain-text set with tags separated by slashes.
 
 It DOES NOT escape slashes so be aware of that.
+
+The files will be grabbed in xml/ directory and will end up in txt/ subdirectory.
 """
 
 def convert_file(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    f = open(filename + ".txt", "wb")
+    out_filename = os.path.join("txt", os.path.basename(filename)) + ".txt"
+    f = open(out_filename, "wb")
+    print("Converting %s => %s" % (filename, out_filename))
 
     for div in root:
         for paragraph in div:
@@ -20,18 +28,18 @@ def convert_file(filename):
                     tag = element.tag[-1]
                     if tag == 'w':
                         f.write(element.text.encode('utf-8'))
-                        f.write('/')
-                        f.write(element.attrib["msd"])
+                        f.write(bytes(b'/'))
+                        f.write(bytes(element.attrib["msd"], encoding="utf-8"))
                     elif tag == 'S':
-                        f.write(' ')
+                        f.write(bytes(b' '))
                     elif tag == 'c':
                         f.write(element.text.encode('utf-8'))
-            f.write('\n\n')
+            f.write(bytes(b'\n\n'))
     f.close()
 
 
-files = glob.glob('jos1M*.xml')
-
-for f in files:
-    print "Converting", f
-    convert_file(f)
+if __name__ == "__main__":
+    files = glob.glob('xml/jos1M*.xml')
+    for f in files:
+        convert_file(f)
+    print("Complete.")
