bc756bd8e0ea5c70ffd9ec4ad02d5daae21db3f3
Jernej Virag <jernej@virag.si>
2020-12-24 22:11:27 +0100

    Update setup.py and bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name="SloPOS",
-      version="1.0",
+      version="1.1",
       description="Part of speech tagger for Slovenian (SI) language based on NLTK",
       package_data={'slopos': ["sl-tagger.pickle"]},
       license="LGPL",
@@ -14,8 +20,12 @@ setup(name="SloPOS",
           "Intended Audience :: Developers",
           "Topic :: Software Development :: Libraries :: Python Modules",
           "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
           "Topic :: Text Processing :: Linguistic"
       ],
       install_requires=["nltk"],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       packages=["slopos"])
 
